dc389f898f5e27b654ddb1939441aae9bb8c19d0
Ankit77760 <ankitm7972@gmail.com>
2024-05-30 19:13:27 +0530

    May Solutions

---
+++ May_2024/30_.cpp
@@ -0,0 +1,57 @@
+//1442. Count Triplets That Can Form Two Arrays of Equal XOR
+/*
+Given an array of integers arr.
+
+We want to select three indices i, j and k where (0 <= i < j <= k < arr.length).
+
+Let's define a and b as follows:
+
+a = arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1]
+b = arr[j] ^ arr[j + 1] ^ ... ^ arr[k]
+Note that ^ denotes the bitwise-xor operation.
+
+Return the number of triplets (i, j and k) Where a == b.
+
+ 
+
+Example 1:
+
+Input: arr = [2,3,1,6,7]
+Output: 4
+Explanation: The triplets are (0,1,2), (0,2,2), (2,3,4) and (2,4,4)
+Example 2:
+
+Input: arr = [1,1,1,1,1]
+Output: 10
+ 
+
+Constraints:
+
+1 <= arr.length <= 300
+1 <= arr[i] <= 108
+*/
+
+//Solutions
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        int n=arr.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int xor1=0;
+                for(int k=i;k<j;k++){
+                    xor1=xor1^arr[k];
+                }
+                int xor2=0;
+                for(int k=j;k<n;k++){
+                    xor2=xor2^arr[k];
+                    if(xor2==xor1){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};

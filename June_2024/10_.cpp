64296f20430e7bcb39814245e8f0f28ebf01b80c
Ankit77760 <ankitm7972@gmail.com>
2024-06-10 20:31:22 +0530

    June Solutions

---
+++ June_2024/10_.cpp
@@ -0,0 +1,62 @@
+//1051. Height Checker
+/*
+A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
+
+You are given an integer array heights representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+
+Return the number of indices where heights[i] != expected[i].
+
+ 
+
+Example 1:
+
+Input: heights = [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+heights:  [1,1,4,2,1,3]
+expected: [1,1,1,2,3,4]
+Indices 2, 4, and 5 do not match.
+Example 2:
+
+Input: heights = [5,1,2,3,4]
+Output: 5
+Explanation:
+heights:  [5,1,2,3,4]
+expected: [1,2,3,4,5]
+All indices do not match.
+Example 3:
+
+Input: heights = [1,2,3,4,5]
+Output: 0
+Explanation:
+heights:  [1,2,3,4,5]
+expected: [1,2,3,4,5]
+All indices match.
+ 
+
+Constraints:
+
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
+*/
+
+//Solutions
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count=0;
+        int n=heights.size();
+        vector<int>copy;
+        for(int i=0;i<n;i++){
+            copy.push_back(heights[i]);
+        }
+        sort(copy.begin(),copy.end());
+        for(int i=0;i<n;i++){
+            if(heights[i]!=copy[i]){
+                count++;
+            }
+        }
+        return count;
+        
+    }
+};
